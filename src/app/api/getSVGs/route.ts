cee89b30101c4d97eabdecbec94f7e0d1f100552
Tim Arnold <tim-arnold@outlook.de>
2024-12-11 11:21:46 +0100

    Added route

---
+++ src/app/api/getSVGs/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest } from "next/server";
+import AuthShaperTools from "@/app/api/AuthShaperTools";
+
+export async function GET(request: NextRequest) {
+
+    const user = request.nextUrl.searchParams.get('user') ?? '';
+    const pw = request.nextUrl.searchParams.get('pw') ?? '';
+    const jwt = await AuthShaperTools(user, pw);
+
+       const svgs = await fetch("https://api.shapertools.com/files/userspace/search?limit=50&path=%2F&sort=modified%3A-1&spaceType=userspace", {
+            method: "GET",
+            headers: {
+              "Accept": "*/*",
+              "Accept-Language": "de-DE,de;q=0.9",
+              "Authorization": "Bearer "+jwt,
+              "Cache-Control": "no-cache",
+              "Connection": "keep-alive",
+              "Origin": "https://hub.shapertools.com",
+              "Pragma": "no-cache",
+              "Referer": "https://hub.shapertools.com/",
+              "Sec-Fetch-Dest": "empty",
+              "Sec-Fetch-Mode": "cors",
+              "Sec-Fetch-Site": "same-site",
+              "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+              "X-ApiVersion": "3.0.0",
+              "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+              "sec-ch-ua-mobile": "?0",
+              "sec-ch-ua-platform": "\"Windows\""
+            },
+            redirect: "follow"
+          })
+            .then((response) => response.json())
+            .catch(console.error);
+          
+      
+
+      return new Response(JSON.stringify(svgs.results), {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+          "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          "Access-Control-Allow-Credentials": "true"
+        }
+      });
+}
---
+++ src/components/SelectSVGFromShaper.tsx
@@ -0,0 +1,135 @@
+"use client";
+import { useEffect, useState } from "react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import { Button } from "./ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import Image from "next/image";
+import {ArrowRight} from "lucide-react";
+
+class SVGModel {
+    _id: string;
+    type: string;
+    path: string;
+    name: string;
+    blob: string;
+    blobs: string[];
+    size: number;
+    created: string;
+    modified: string;
+    thumbnailURI: string;
+
+    constructor(data: any) {
+        this._id = data._id;
+        this.type = data.type;
+        this.path = data.path;
+        this.name = data.name;
+        this.blob = data.blob;
+        this.blobs = data.blobs;
+        this.size = data.size;
+        this.created = data.created;
+        this.modified = data.modified;
+        this.thumbnailURI = data.thumbnailURI;
+    }
+}
+
+function svgEntry(svgModel: SVGModel) {
+    return <Card className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <CardHeader className="flex flex-col gap-8">
+            <CardContent className={"flex flex-row gap-4 items-center  w-full p-0"}>
+                <div className={"flex flex-row gap-4 items-center p-0"}>
+                    <Image src={svgModel.thumbnailURI} alt={svgModel.name} width={100} height={100} />
+                    <p className="text-primary text-xl font-bold">{svgModel.name}</p>
+                </div>
+                <ArrowRight className="w-6 h-6" />
+            </CardContent>
+        </CardHeader>
+    </Card>
+}
+
+
+export default function SelectSVGFromShaper(props: {onSelect: (svgModel: File) => void, pw: string, user: string}) {
+    const [svgModels, setSvgModels] = useState<SVGModel[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [openState, setOpenState] = useState(false);
+    const [downloading, setDownloading] = useState(false);
+
+
+    function changeOpenState(newState: boolean) {
+        setOpenState(newState);
+        if(!newState) {
+            setDownloading(false);
+            setSvgModels([]);
+            setLoading(false);
+        }else{
+            loadData();
+        }
+
+    }
+
+   async function selectSvgModel(svgModel: SVGModel) {
+        setOpenState(false);
+        setDownloading(true);
+        const response = await fetch("/api/downloadSVGFromShapertools?blobKey="+svgModel.blob+"&pw="+props.pw+"&user="+props.user).then((response) => response.json());
+        const downloadURL = await response.downloadURL;
+        //download file from url
+        const file = await fetch(downloadURL).then((response) => response.blob()).then((blob) => new File([blob], svgModel.name, {type: "image/svg+xml"}));
+        props.onSelect(file);
+        setDownloading(false);
+
+    }
+
+    function loadData() {
+        setLoading(true);
+        fetch("/api/getSVGs"+"?pw="+props.pw+"&user="+props.user)
+            .then((response) => response.json())
+            .then((data) => {
+                setSvgModels(data);
+                setLoading(false);
+            });
+    }
+
+
+
+    return (
+        <Dialog open={openState} onOpenChange={changeOpenState}>
+            <DialogTrigger asChild><Button disabled={downloading}>{downloading? "Loading...": "Import from Shapertools"}</Button></DialogTrigger>
+            <DialogContent className={"max-h-[60vh] max-w-[50vw] overflow-y-hidden"}>
+                <DialogHeader>
+                    <DialogTitle>Select file from Shapertools</DialogTitle>
+                    <DialogDescription asChild>
+                        <div className={"max-h-[50vh] max-w-[50vw]"}>
+                            Select the file you want to import from Shapertools.
+                            <br/><br/>
+                            {loading && <p>Loading...</p>}
+                            {!loading && svgModels.length === 0 && <p>No files found.</p>}
+                            <div className={"overflow-y-scroll flex flex-col gap-4 w-full h-[90%] flex-1 justify-stretch p-8 cursor-pointer"}>
+                                {
+                                    !loading && svgModels.length > 0 &&
+                                    svgModels.map((svgModel) => (
+                                        <div key={svgModel._id} onClick={() => selectSvgModel(svgModel)}>
+                                            {svgEntry(svgModel)}
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
+}
